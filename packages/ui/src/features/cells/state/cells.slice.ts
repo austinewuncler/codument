d41f17b26a5e8aad52318d6730a3ff1e40004ecc
Austine Were <austinewuncler@gmail.com>
2022-12-06 14:47:05 +0300

    feat(ui): display default cells if file is new

--- packages/ui/src/features/cells/state/cells.slice.ts
+++ packages/ui/src/features/cells/state/cells.slice.ts
@@ -70,7 +70,30 @@ const cellsSlice = createSlice({
         (state, { payload }) => {
           const cells = payload;
           state.loading = false;
-          state.data = cellsAdapter.addMany(state.data, cells);
+          if (cells.length > 0)
+            state.data = cellsAdapter.addMany(state.data, cells);
+          else {
+            const defaultCells: Cell[] = [
+              {
+                content:
+                  '# Codument\n\nThis is an interactive coding environment. You can write JavaScript, see it executed, and write comprehensive documentation using markdown.\n\n- Click any markdown cell (including this one) to edit it.\n- The code in each code editor share the same scope. If you define a variable in cell #1, you can refer to it in any following cell.\n- You can show any React component, string, number or anything else by calling the `show` function. This is a function build into this environment. Call show multiple times to show multiple values.\n- Reorder or delete cells using the buttons to the top-right corner of each cell.\n- Insert new cells by hovering on the space between cells.\n\nAll of your changes get saved to the file you opened Codument with. For example if you ran\n\n```bash\nnpx -p @codument/cli codument serve mycode.json\n```\n\nor\n\n```bash\nyarn dlx -p @codument/cli codument serve mycode.json\n```\n\nall of the code and markdown will be saved to `mycode.json`.',
+                id: nanoid(),
+                type: 'markdown',
+              },
+              {
+                content:
+                  'import \'bulma/css/bulma.css\';\nimport { useState } from \'react\';\n\nconst Counter = () => {\n  const [count, setCount] = useState(0);\n\n  return (\n    <div className="box is-flex is-flex-direction-column is-align-items-center">\n      <span>{count}</span>\n      <button\n        onClick={() => setCount((p) => p + 1)}\n        className="button is-primary"\n      >\n        Increase\n      </button>\n    </div>\n  );\n};',
+                id: nanoid(),
+                type: 'code',
+              },
+              {
+                content: 'show(<Counter/>)',
+                id: nanoid(),
+                type: 'code',
+              },
+            ];
+            state.data = cellsAdapter.addMany(state.data, defaultCells);
+          }
         }
       ),
 });
